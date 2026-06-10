454bfe9589e6ac6aa1107443232b76d24f1d0011
Minh <223230201+BlackBeard1720@users.noreply.github.com>
2026-06-10 11:12:16 +0700

    add route find by id

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,15 +1,36 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
+  private products = [
+    {
+      id: 1,
+      name: 'Laptop',
+      price: 1000,
+    },
+    {
+      id: 2,
+      name: 'Mouse',
+      price: 500,
+    },
+    {
+      id: 3,
+      name: 'PC',
+      price: 2000,
+    },
+    {
+      id: 4,
+      name: 'Phone',
+      price: 400,
+    },
+  ];
   @Get()
   findAll() {
-    return [
-      {
-        id: 1,
-        name: 'Laptop',
-        price: 1000,
-      },
-    ];
+    return this.products;
+  }
+  //route find product by id
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.products.find((product) => product.id === Number(id));
   }
 }
